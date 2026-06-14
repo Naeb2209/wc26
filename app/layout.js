2631ba05eac004da0643f761058b4564b6eae0ba
Bean <hoanganhck113@gmail.com>
2026-06-14 21:18:38 +0700

    fix load icon

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;700;800&family=Hanken+Grotesk:wght@600;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
+        />
+      </head>
       <body className="bg-background text-on-background antialiased min-h-screen flex flex-col">
         <Nav />
         <div className="flex-grow flex flex-col">{children}</div>
